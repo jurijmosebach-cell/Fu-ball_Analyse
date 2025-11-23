3d390097e8f7968abcf8f957955841db97304ba1
jurijmosebach-cell <jurij.mosebach@gmail.com>
2025-11-23 08:19:43 +0100

    Fix comment syntax in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/ server-football-data-only.js - OPTIMIZED FOR FOOTBALL-DATA.ORG
+// server-football-data-only.js - OPTIMIZED FOR FOOTBALL-DATA.ORG
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
